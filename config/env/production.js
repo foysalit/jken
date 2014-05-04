695899e7faad68953f171aeb80d280318eb66265
Foysal Ahamed <cfaion341@gmail.com>
2014-05-04 19:31:38 +0200

    OS env var

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,8 @@
 module.exports = {
     db: {
         name: process.env.APP_DB_NAME,
-        password: APP_DB_PASSWORD,
-        username: APP_DB_USERNAME,
+        password: process.env.APP_DB_PASSWORD,
+        username: process.env.APP_DB_USERNAME,
         host: process.env.OPENSHIFT_MYSQL_DB_HOST,
         initiate: false
     },
