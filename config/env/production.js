a22fe0071939165c0748b0c2380023f2023f342e
Foysal Ahamed <cfaion341@gmail.com>
2014-05-04 17:28:42 +0200

    prod env vars in config

--- config/env/production.js
+++ config/env/production.js
@@ -1,12 +1,13 @@
 module.exports = {
     db: {
-        name: "",
-        password: "",
-        username: "",
+        name: process.env.APP_DB_NAME,
+        password: APP_DB_PASSWORD,
+        username: APP_DB_USERNAME,
         host: process.env.OPENSHIFT_MYSQL_DB_HOST,
         initiate: false
     },
     app: {
         name: "Dashboard - Production"
-    }
+    },
+    port: process.env.OPENSHIFT_NODEJS_PORT
 }
