4a493546afbb75fb31553609161d55291ca00bee
Foysal Ahamed <cfaion341@gmail.com>
2014-05-12 23:01:42 +0200

    added error page and hid the sign up button

--- config/routes.js
+++ config/routes.js
@@ -13,8 +13,8 @@ exports.init = function(app, passport, auth) {
     app.get('/login', users.signin);
     //sign up is closed for now
     //app.get('/signup', users.signup);
-    app.get('/logout', users.signout);
-    app.get('/users/me', users.me);
+    app.get('/logout', auth.requiresLogin, users.signout);
+    app.get('/users/me', auth.requiresLogin, users.me);
 
     // Setting up the users api
     //sign up is closed for now
@@ -30,23 +30,23 @@ exports.init = function(app, passport, auth) {
     app.param('userId', users.user);
 
     // Class Routes
-    app.get('/klasses', klasses.all);
+    app.get('/klasses', auth.requiresLogin, klasses.all);
     app.post('/klasses', auth.requiresLogin, klasses.create);
-    app.get('/klasses/:klassId', klasses.show);
+    app.get('/klasses/:klassId', auth.requiresLogin, klasses.show);
     app.put('/klasses/:klassId', auth.requiresLogin, auth.common.hasAuthorization, klasses.update);
     app.del('/klasses/:klassId', auth.requiresLogin, auth.common.hasAuthorization, klasses.destroy);
 
     // Category Routes
-    app.get('/categories', categories.all);
+    app.get('/categories', auth.requiresLogin, categories.all);
     app.post('/categories', auth.requiresLogin, categories.create);
-    app.get('/categories/:categoryId', categories.show);
+    app.get('/categories/:categoryId', auth.requiresLogin, categories.show);
     app.put('/categories/:categoryId', auth.requiresLogin, auth.common.hasAuthorization, categories.update);
     app.del('/categories/:categoryId', auth.requiresLogin, auth.common.hasAuthorization, categories.destroy);
 
     // Category Routes
-    app.get('/transactions', transactions.all);
+    app.get('/transactions', auth.requiresLogin, transactions.all);
     app.post('/transactions', auth.requiresLogin, transactions.create);
-    app.get('/transactions/:transactionId', transactions.show);
+    app.get('/transactions/:transactionId', auth.requiresLogin, transactions.show);
     app.put('/transactions/:transactionId', auth.requiresLogin, auth.common.hasAuthorization, transactions.update);
     app.del('/transactions/:transactionId', auth.requiresLogin, auth.common.hasAuthorization, transactions.destroy);
 
