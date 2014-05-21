5112205b05c377f51b90f49f3324df4a07db6176
foysal <cfaion341@gmail.com>
2014-05-21 16:47:44 +0100

    adjusted db import

--- app/lib/dbImport.js
+++ app/lib/dbImport.js
@@ -3,9 +3,10 @@ var _       = require('lodash'),
     Db      = require('../../config/sequelize');
 
 setTimeout(function(){
-    Async.series([importClasses, importCategories, importTransactions], function (err, result) {
+    Async.series([importEntities, importCategories, importTransactions], function (err, result) {
         if(err){
             console.log('error in the process');
+            console.log(err);
         }else{
             _.each(result, function (r) {
                 console.log(r);
@@ -14,77 +15,128 @@ setTimeout(function(){
     });
 }, 5000);
 
-var importTransactions = function(callback){
-    Db.Klass.findAll({include: [Db.Category, Db.User]}).success(function(ks){
-        Db.sequelize.query('select * from transactions').success(function(ts){
-            _.each(ts, function(t){
-                if(t.class == '') t.class = 'N/A';
-                if(t.category == '') t.category = 'N/A';
-
-                _.each(ks, function (k) {
-                    if(k.name != t.class) return;
-                    
-                    _.each(k.categories, function(c){
-                        if(c.name != t.category) return;
-                        
-                        Db.Transaction.create({
-                            CategoryId: c.id,
-                            KlassId: k.id,
-                            UserId: 1,
-                            account: t.account,
-                            number: t.number,
-                            payee: t.payee,
-                            date: t.date,
-                            cleared: t.cleared == -1 ? '0' : '1',
-                            amount: t.amount,
-                            description: t.description
-                        });
-                    });
-                });
+var importTransactions = function(endImport){
+    Async.parallel({
+        entities : function (callback) {
+            Db.Klass.findAll().success(function (klasses) {
+                callback(null, klasses);
+            }).failure(function (err) {
+                callback(err);
             });
+        },
+        categories: function (callback) {
+            Db.Category.findAll().success(function (categories) {
+                callback(null, categories);
+            }).failure(function (err) {
+                callback(err);
+            });
+        },
+        transactions: function(callback) {
+             Db.sequelize.query('select * from transactions').success(function (transactions) {
+                callback(null, transactions);
+            }).failure(function (err) {
+                callback(err);
+            });
+        }
+    }, function (err, data) {
+        if(err){
+            console.log('retreival error', err);
+            return;
+        }
 
-            setTimeout(function(){
-                callback(null, 'transactions inserted');
-            }, 40000);
+        Async.each(data.transactions, function (t, nextEntry) {
+            var entityName      = t.entity.length > 0 ? t.entity : 'N/A',
+                categoryName    = t.category.length > 0 ? t.category : 'N/A',
+                category      = _.find(data.categories, {name: categoryName}),
+                klass        = _.find(data.entities, function(e){
+                    return e.name.toUpperCase() == entityName.toUpperCase();
+                });
+
+            if(typeof category === 'undefined' || typeof klass === 'undefined'){
+                nextEntry(t);
+                return;
+            }
+
+            Db.Transaction.create({
+                CategoryId: category.id,
+                KlassId: klass.id,
+                UserId: 1,
+                account: t.account,
+                number: t.number,
+                payee: t.payee,
+                date: t.date,
+                cleared: t.cleared == -1 ? '0' : '1',
+                amount: t.amount,
+                description: t.description
+            }).success(function () {
+                nextEntry();
+            }).failure(function (err) {
+                nextEntry(err);
+            });
+        }, function (err) {
+            if(err){
+                endImport(err);
+                return;
+            }
+
+            endImport(null, 'transactions imported');
         });
     });
 };
 
-var importClasses = function(callback){
-    Db.sequelize.query('select * from transactions group by class').success(function(ks){
-        _.each(ks, function(c){
-            if(c.class.length > 0){
-                Db.Klass.create({name: c.class, UserId: 1});
-            }else{
-                Db.Klass.create({name: 'N/A', UserId: 1});
-            }
-        });
+var importEntities = function(callback){
+    Db.sequelize.query('select entity from transactions group by entity').success(function(entities){
+        Async.each(entities, function(c, nextEntry){
+            if(c.entity.length > 0)
+                var name = c.entity;
+            else
+                var name = 'N/A';
 
-        setTimeout(function(){
-            callback(null, 'classes inserted');
-        }, 5000);
+            Db.Klass.create({name: name, UserId: 1})
+                .success(function () {
+                    nextEntry();
+                })
+                .failure(function (err) {
+                    nextEntry(err);
+                });
+        }, function (err) {
+            if(err){
+                console.log('error importing entities', err);
+                callback('error importing entities');
+                return;
+            }
+
+            callback(null, 'entities inserted');
+        });
+    }).failure(function (err) {
+        console.log(err);
+        callback(err);
     });
 };
 
 var importCategories = function(callback){
-    Db.sequelize.query('select category, class from transactions group by category, class').success(function(cats){
-        Db.Klass.findAll().success(function(klasses){
-            _.each(cats, function(c){
-                if(c.class.length <= 0) c.class = 'N/A';
+    Db.sequelize.query('select category from transactions group by category').success(function(cats){
+        Async.each(cats, function (c, nextEntry) {
+           
+            if(c.category.length <= 0) var name = 'N/A';
+            else var name = c.category;
+            
+            Db.Category.create({name: name, UserId: 1})
+                .success(function () {
+                    nextEntry();
+                })
+                .failure(function (err) {
+                    nextEntry(err);
+                });
+         
+        }, function (err) {
+            if(err){
+                console.log('error importing cats', err);
+                callback('error importing cats');
+                return;
+            }
 
-                if(c.category.length <= 0) name = 'N/A';
-                else name = c.category;
-
-                klass = _.find(klasses, {name: c.class});
-                
-                if(typeof klass != 'undefined'){
-                    Db.Category.create({name: name, KlassId: klass.id, UserId: 1});
-                }
-            });
-                
-            setTimeout(function(){
-                callback(null, 'categories inserted');
-            }, 10000);
+            callback(null, 'categories inserted');
         });
     });
 };
